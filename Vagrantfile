c125ca7c16fdc91fd0688aeec4e413f6b7ea020e
luis edymerchk <edyhack@gmail.com>
2014-06-20 20:26:00 -0500

    adding ruby using rbenv, postgres, node and others

--- Vagrantfile
+++ Vagrantfile
@@ -7,4 +7,8 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
   config.vm.box = "hashicorp/precise32"
 
+  config.vm.network "forwarded_port", guest: 3000, host: 3000
+
+  config.vm.provision "shell", path: "script.sh"
+
 end
---
+++ script.sh
@@ -0,0 +1,18 @@
+
+
+sudo apt-get -y update
+
+sudo apt-get install -y git nodejs postgresql-9.3 postgresql-server-dev-9.3 libpq-dev
+
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+export RBENV_ROOT="${HOME}/.rbenv"
+export PATH="${RBENV_ROOT}/bin:${PATH}"
+export PATH="${RBENV_ROOT}/shims:${PATH}"
+rbenv install 2.1.1
+rbenv global 2.1.1
+gem install bundler
+rbenv rehash
